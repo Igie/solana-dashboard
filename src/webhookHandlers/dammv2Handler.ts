86142398660d994b7dc3066b80de61a45e773470
Igie <shadriu@gmail.com>
2025-08-14 10:36:56 +0000

    webhook

--- src/webhookHandlers/dammv2Handler.ts
+++ src/webhookHandlers/dammv2Handler.ts
@@ -1,11 +1,13 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function dammv2Handler(req: VercelRequest, res: VercelResponse) {
-  const name = req.query.name ?? 'World';
-  
-  console.log(req.body);
+  try {
+    console.log(req.body);
 
-  res.writeHead(200);
-  res.write(`Hello ${name}!`);
-  res.end();
+    res.writeHead(200);
+    res.write(`Hello!`);
+    res.end();
+  } catch (e) {
+    console.log(e)
+  }
 }
