8f133c57fb638cdbeb6d1bdc145ea1ea13d89188
Igie <shadriu@gmail.com>
2025-08-15 13:03:28 +0000

    fixed files

--- api/dammv2.ts
+++ api/dammv2.ts
@@ -1,4 +1,12 @@
-export async function GET(request: Request) {
-  console.log(request.body)
-  return Response.json("Hello!");
+export const config = {
+  runtime: "nodejs"
+};
+
+import { VercelRequest, VercelResponse } from '@vercel/node';
+ 
+export const handler = (req: VercelRequest, res: VercelResponse) => {
+  const name = req.query.name ?? 'World';
+  res.writeHead(200);
+  res.write(`Hello ${name}!`);
+  res.end();
 }
