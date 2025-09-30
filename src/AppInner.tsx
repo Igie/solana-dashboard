03fdcc33f655c976c26a34af3bcb41921433be3e
Igie <shadriu@gmail.com>
2025-09-30 17:03:57 +0000

    wiiiide

--- src/AppInner.tsx
+++ src/AppInner.tsx
@@ -89,7 +89,7 @@ const AppInner: React.FC<AppInnerProps> = ({
       </header>
 
       <main className="flex flex-col h-[calc(100vh-42px)] pb-[calc(4rem+var(10px))] w-full px-4 py-2">
-        <div className="w-full max-w-screen-2xl mx-auto">
+        <div className="w-full max-w-screen mx-auto">
           <ActiveComponent goToPoolPage={goToPoolPage} tokenAMintParam={tokenAMint} tokenAAmountParam={tokenAAmount} />
         </div>
       </main>
